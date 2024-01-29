4f65feeab4e297ddd4e379cad302e6a55baef6de
Miguel Figueiredo <miguel.ciancio@outlook.com>
2024-01-29 10:51:29 +0000

    Done Submit Button Command.

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ class App:
         self.root = Tk()
         self.root.geometry("400x400")
 
-        # ===== Frame Widgets Declaration Area ===== #
+        # ===== Widgets Declaration Area ===== #
         self.select_frame = Frame(self.root)
         self.select_frame.pack(fill=BOTH)
 
         self.display_frame = Frame(self.root)
         self.display_frame.pack(fill=BOTH)
+
+        # ===== Variables Area ===== #
+        self.select_list = []
+        self.value_inside = StringVar()
 
     def main_window(self):
         """
@@ -25,14 +29,13 @@ class App:
         label1.grid(row=0, column=0, ipadx=10)
 
         # Create Entry Combo box.
-        options_list = [1, 2, 3, 4, 5, "Hello", "World"]
+        self.select_list = [1, 2, 3, 4, 5, "Hello", "World"]
 
-        value_inside = StringVar(self.root)
-        value_inside.set("Select an Option")
+        self.value_inside.set("Select an Option")
 
-        entry_combobox_1 = OptionMenu(self.select_frame, value_inside, *options_list)
-        entry_combobox_1.configure(width=21)
-        entry_combobox_1.grid(row=0, column=1, pady=10)
+        entry_combobox = OptionMenu(self.select_frame, self.value_inside, *self.select_list)
+        entry_combobox.configure(width=21)
+        entry_combobox.grid(row=0, column=1, pady=10)
 
         # Create Buttons.
         submit_button = Button(self.select_frame, text="Submit", command=self.show_air_quality)
@@ -44,8 +47,12 @@ class App:
         clear_button.configure(width=20)
 
     def show_air_quality(self):
-        label2 = Label(self.display_frame, text="This is a Dummy Text!!!")
-        label2.grid(row=0, column=0, columnspan=2)
+        self.clear_all()
+        if self.value_inside.get() == "Select an Option":
+            pass
+        else:
+            label2 = Label(self.display_frame, text=self.value_inside.get())
+            label2.grid(row=0, column=0, columnspan=2)
 
     def clear_all(self):
         label3 = Label(self.display_frame, text="")
