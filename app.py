544146118a0366b0eaec94857e7e712d380e77f2
Miguel Figueiredo <miguel.ciancio@outlook.com>
2024-01-29 09:54:04 +0000

    Done Clear Button Command.

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from tkinter import *
 
 
 class App:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self):
         self.root = Tk()
         self.root.geometry("400x400")
+
+        # ===== Frame Widgets Declaration Area ===== #
+        self.select_frame = Frame(self.root)
+        self.select_frame.pack(fill=BOTH)
+
+        self.display_frame = Frame(self.root)
+        self.display_frame.pack(fill=BOTH)
 
     def main_window(self):
         """
@@ -13,12 +19,8 @@ class App:
         :return: nothing.
         """
         # ===== First Frame Area =====#
-        # Create Frame.
-        select_frame = Frame(self.root)
-        select_frame.pack(fill=BOTH)
-
         # Create Label.
-        label1 = Label(select_frame, text="Select Options")
+        label1 = Label(self.select_frame, text="Select Options")
         label1.configure(width=10)
         label1.grid(row=0, column=0, ipadx=10)
 
@@ -28,29 +30,31 @@ class App:
         value_inside = StringVar(self.root)
         value_inside.set("Select an Option")
 
-        entry_combobox_1 = OptionMenu(select_frame, value_inside, *options_list)
+        entry_combobox_1 = OptionMenu(self.select_frame, value_inside, *options_list)
         entry_combobox_1.configure(width=21)
         entry_combobox_1.grid(row=0, column=1, pady=10)
 
         # Create Buttons.
-        submit_button = Button(select_frame, text="Submit")
+        submit_button = Button(self.select_frame, text="Submit", command=self.show_air_quality)
         submit_button.grid(row=1, column=0, padx=5, pady=20, ipadx=10, sticky=W+E)
         submit_button.configure(width=20)
 
-        clear_button = Button(select_frame, text="Clear")
+        clear_button = Button(self.select_frame, text="Clear", command=self.clear_all)
         clear_button.grid(row=1, column=1, padx=24, ipadx=10, sticky=W)
         clear_button.configure(width=20)
 
-        # ===== Second Frame Area =====#
-        display_frame = Frame(self.root)
-        display_frame.pack(fill=BOTH)
+    def show_air_quality(self):
+        label2 = Label(self.display_frame, text="This is a Dummy Text!!!")
+        label2.grid(row=0, column=0, columnspan=2)
 
+    def clear_all(self):
+        label3 = Label(self.display_frame, text="")
+        label3.grid(row=0, column=0, columnspan=2, sticky=W+E)
 
     def run_window(self):
         self.root.mainloop()
 
 
-window = None
-app = App(window)
-app.main_window()
-app.run_window()
+MyApp = App()
+MyApp.main_window()
+MyApp.run_window()
